fe4f2c2d8bd33665c2965e745bdc4b6596d561d9
Felix Böhm <me@feedic.com>
2012-02-11 21:42:48 +0100

    Added missing require

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-var t = parse("*#foo > elem[ class ~= bar i][class$=bAz i]:not([id*=2])")({
+var parse = require("./");
+
+var t = parse('doo, *#foo > elem.bar[class$=bAz i]:not([ id *= "2" ])')({
     name: "elem",
     attribs: { class: "bar baz" },
     parent: {
