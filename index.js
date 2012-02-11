f0128e429f99b73376344926eefc85bbc8ce1b0f
Felix Böhm <me@feedic.com>
2012-02-11 16:35:05 +0100

    Moved test function to test.js

--- index.js
+++ index.js
@@ -195,15 +195,3 @@ var parse = function(selector) {
 };
 
 if (typeof module === "object") module.exports = parse;
-
-/*
-var t = parse("*#foo > elem[ class ~= bar i][class$=bAz i]:not([id*=2])")({
-	name: "elem",
-	attribs: {class:"bar baz"},
-	parent: {
-		name: "elem",
-		attribs: {id: "foo"}
-	}
-});
-console.log("result", !!t)
-*/
