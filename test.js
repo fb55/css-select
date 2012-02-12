2a0ec9853d4895fe856eeeeed97026885b320d4f
Felix Böhm <me@feedic.com>
2012-02-12 14:12:19 +0100

    Restructured test, added performance test
    Performance test only runs when npm module ben is installed

--- test.js
+++ test.js
@@ -1,11 +1,28 @@
-var parse = require("./");
-
-var t = parse('doo, *#foo > elem.bar[class$=bAz i]:not([ id *= "2" ])')({
-    name: "elem",
-    attribs: { class: "bar baz" },
-    parent: {
+var parse = require("./"),
+    testString = 'doo, *#foo > elem.bar[class$=bAz i]:not([ id *= "2" ])',
+    testDoc = {
         name: "elem",
-        attribs: { id: "foo" }
-    }
-});
-console.log("result:", !!t);
+        attribs: { class: "bar baz" },
+        parent: {
+            name: "elem",
+            attribs: { id: "foo" }
+        }
+    };
+
+console.log("result:", !!parse(testString)(testDoc));
+
+try {
+    console.log(
+        "Parsing took:",
+        require("ben")(1e5, function() {
+            parse(testString);
+        }) * 1e3
+    );
+    parse = parse(testString);
+    console.log(
+        "Executing took:",
+        require("ben")(1e6, function() {
+            parse(testDoc);
+        }) * 1e3
+    );
+} catch (e) {}
