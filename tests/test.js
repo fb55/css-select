876a9afe4d6a6904a2b7b91be60322e35798d5d5
Felix Böhm <me@feedic.com>
2012-02-17 11:49:24 +0100

    Moved test.js to tests dir, moved benchmark to bench.js

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+var parse = require("../"),
+    testString = 'doo, *#foo > elem.bar[class$=bAz i]:not([ id *= "2" ])',
+    testDoc = require("./doc.json");
+
+console.log("result:", !!parse(testString)(testDoc));
+
+//TODO
