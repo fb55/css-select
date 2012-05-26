73692a4d00d70e1dbd8330df51044804b018d31c
Felix Böhm <me@feedic.com>
2012-05-26 14:19:19 +0200

    deleted tests.js
    it was _pretty_ basic & stopped being useful

--- tests/test.js
+++
@@ -1,6 +0,0 @@
-var CSSselect = require("../"),
-	testString = "doo, *#foo > elem.bar[class$=bAz i]:not([ id *= \"2\" ]) tag",
-	helper = require("./helper.js"),
-	dom = helper.getDefaultDom();
-
-console.log("result:", CSSselect.iterate(dom, testString).length);
