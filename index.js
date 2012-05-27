b45c7ca30188ec561e86193106f59993c372b06b
Felix Böhm <me@feedic.com>
2012-05-27 10:55:38 +0200

    improved use of primitive functions
    will now return much faster when the selector can't return true

--- index.js
+++ index.js
@@ -49,6 +49,14 @@ function getText(elem){
 var filters = {
 	not: function(next, select){
 		var func = parse(select);
+
+		if(func === falseFunc){
+			if(next === rootFunc) return trueFunc;
+			else return next;
+		}
+		if(func === trueFunc) return falseFunc;
+		if(func === rootFunc) return falseFunc;
+
 		return function(elem){
 			if(!func(elem)) return next(elem);
 		};
@@ -160,8 +168,13 @@ var filters = {
 	},
 	"nth-child": function(next, rule){
 		var func = getNCheck(rule);
+
 		if(func === null) return next;
-		if(func === false) return falseFunc;
+		if(func === falseFunc) return func;
+		if(func === trueFunc){
+			if(next === rootFunc) return func;
+			else return next;
+		}
 
 		return function(elem){
 			if(func(getIndex(elem))) return next(elem);
@@ -169,8 +182,13 @@ var filters = {
 	},
 	"nth-last-child": function(next, rule){
 		var func = getNCheck(rule);
+
 		if(func === null) return next;
-		if(func === false) return falseFunc;
+		if(func === falseFunc) return func;
+		if(func === trueFunc){
+			if(next === rootFunc) return func;
+			else return next;
+		}
 
 		return function(elem){
 			var siblings = getSiblings(elem);
@@ -187,8 +205,13 @@ var filters = {
 	},
 	"nth-of-type": function(next, rule){
 		var func = getNCheck(rule);
+
 		if(func === null) return next;
-		if(func === false) return falseFunc;
+		if(func === falseFunc) return func;
+		if(func === trueFunc){
+			if(next === rootFunc) return func;
+			else return next;
+		}
 
 		return function(elem){
 			var siblings = getSiblings(elem);
@@ -205,8 +228,13 @@ var filters = {
 	},
 	"nth-last-of-type": function(next, rule){
 		var func = getNCheck(rule);
+
 		if(func === null) return next;
-		if(func === false) return function(){ return false; };
+		if(func === falseFunc) return func;
+		if(func === trueFunc){
+			if(next === rootFunc) return func;
+			else return next;
+		}
 
 		return function(elem){
 			var siblings = getSiblings(elem);
@@ -351,10 +379,10 @@ function getNCheck(formula){
 
 	//when b <= 0, a*n won't be possible for any matches when a < 0
 	//besides, the specification says that no element is matched when a and b are 0
-	if(b < 0 && a <= 0) return false;
+	if(b < 0 && a <= 0) return falseFunc;
 
 	//when b <= 0 and a === 1, they match any element
-	if(b < 0 && a === 1) return null;
+	if(b < 0 && a === 1) return trueFunc;
 
 	//when a is in the range -1..1, it matches any element (so only b is checked)
 	if(a ===-1) return function(pos){ return pos - b <= 0; };
@@ -388,6 +416,10 @@ function checkAttrib(next, name, value){
 }
 
 function rootFunc(){
+	return true;
+}
+
+function trueFunc(){
 	return true;
 }
 
@@ -442,7 +474,7 @@ var generalRules = {
 		};
 	},
 	universal: function(next){
-		if(next === rootFunc) return function(){ return true; };
+		if(next === rootFunc) return trueFunc;
 		return next;
 	},
 
@@ -570,6 +602,7 @@ function parse(selector){
 		var func = rootFunc;
 		for(var i = 0, j = arr.length; i < j; i++){
 			func = generalRules[arr[i].type](func, arr[i]);
+			if(func === falseFunc) return func;
 		}
 		return func;
 	}).filter(function(func){
@@ -604,6 +637,7 @@ CSSselect.pseudos = pseudos;
 
 CSSselect.iterate = function(query, elems){
 	if(typeof query !== "function") query = parse(query);
+	if(query === falseFunc) return [];
 	if(!Array.isArray(elems)) elems = [elems];
 	return iterate(query, elems);
 };
