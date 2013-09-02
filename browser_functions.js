0e9d0d0114820c92c1d31e4e632a72e6adec7c0e
Felix Böhm <me@feedic.com>
2013-09-02 15:23:44 +0200

    enable browserify-cation of the module (based on the browsers DOM)

--- browser_functions.js
+++ browser_functions.js
@@ -1,9 +1,5 @@
-function isElement(elem){
+function isTag(elem){
 	return elem.nodeType === 1;
-}
-function getSiblings(elem){
-	var parent = getParent(elem);
-	return parent && getChildren(parent);
 }
 function getChildren(elem){
 	return elem.childNodes;
@@ -11,25 +7,61 @@ function getChildren(elem){
 function getParent(elem){
 	return elem.parentElement;
 }
-function getAttributeValue(elem, name){
-	return elem.attributes[name].value;
-}
-function hasAttrib(elem, name){
-	return name in elem.attributes;
-}
-function getName(elem){
-	return elem.tagName.toLowerCase();
-}
-//https://github.com/ded/qwery/blob/master/pseudos/qwery-pseudos.js#L47-54
-function getText(elem) {
-	var str = "",
-	    childs = getChildren(elem);
 
-	if(!childs) return str;
+module.exports = {
+	isTag: isTag,
+	getSiblings: function(elem){
+		var parent = getParent(elem);
+		return parent && getChildren(parent);
+	},
+	getChildren: getChildren,
+	getParent: getParent,
+	getAttributeValue: function(elem, name){
+		return elem.attributes[name].value;
+	},
+	hasAttrib: function(elem, name){
+		return name in elem.attributes;
+	},
+	getName: function(elem){
+		return elem.tagName.toLowerCase();
+	},
+	findOne: function findOne(test, arr){
+		var elem = null;
 
-	for(var i = 0; i < childs.length; i++){
-		if(isElem(childs[i]) str += el.textContent || el.innerText || getText(childs[i])
+		for(var i = 0, l = arr.length; i < l && !elem; i++){
+			if(test(arr[i])){
+				elem = arr[i];
+			} else {
+				var childs = getChildren(arr[i]);
+				if(childs && childs.length > 0){
+					elem = findOne(test, childs);
+				}
+			}
+		}
+
+		return elem;
+	},
+	findAll: function findAll(test, elems){
+		var result = [];
+		for(var i = 0, j = elems.length; i < j; i++){
+			if(!isTag(elems[i])) continue;
+			if(test(elems[i])) result.push(elems[i]);
+			var childs = getChildren(elems[i]);
+			if(childs) result = result.concat(findAll(test, childs));
+		}
+		return result;
+	},
+	//https://github.com/ded/qwery/blob/master/pseudos/qwery-pseudos.js#L47-54
+	getText: function getText(elem) {
+		var str = "",
+		    childs = getChildren(elem);
+
+		if(!childs) return str;
+
+		for(var i = 0; i < childs.length; i++){
+			if(isTag(childs[i])) str += elem.textContent || elem.innerText || getText(childs[i]);
+		}
+
+		return str;
 	}
-
-	return s;
-}
+};
