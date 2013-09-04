83d5ae0bf6a71376257f842fc7d9dbb2477592aa
Felix Böhm <me@feedic.com>
2013-09-04 14:57:10 +0200

    [test/helper] use htmparser2's parseDOM method

--- test/tools/helper.js
+++ test/tools/helper.js
@@ -1,28 +1,18 @@
 var htmlparser2 = require("htmlparser2"),
-    Parser = htmlparser2.Parser,
-    Handler = htmlparser2.DomHandler,
     CSSselect = require("../../");
 
 module.exports = {
     CSSselect: CSSselect,
     getFile: function(name) {
-        return module.exports.getDOM(
+        return htmlparser2.parseDOM(
             require("fs")
                 .readFileSync(__dirname + "/docs/" + name)
                 .toString()
         );
     },
-    getDOM: function(data) {
-        var h = new Handler({ refParent: true, ignoreWhitespace: true }),
-            p = new Parser(h);
-
-        p.write(data);
-        p.end();
-
-        return h.dom;
-    },
+    getDOM: htmlparser2.parseDOM,
     getDefaultDom: function() {
-        return module.exports.getDOM(
+        return htmlparser2.parseDOM(
             "<elem id=foo><elem class='bar baz'><tag class='boom'> This is some simple text </tag></elem></elem>"
         );
     }
