a1dd4da5e9df996b41634f3551f798f00cf9dbff
Felix Böhm <me@feedic.com>
2013-09-05 15:59:48 +0200

    [benchmarks] updated API accesses

--- test/tools/slickspeed.js
+++ test/tools/slickspeed.js
@@ -1,10 +1,10 @@
 var helper = require("./helper.js"),
 	doc = helper.getFile("W3C_Selectors.html"),
-	CSSselect = require("../../"),
+	CSSselect = helper.CSSselect,
 	soupselect = require("cheerio-soupselect"),
 	selectors = ["body", "div", "body div", "div p", "div > p", "div + p", "div ~ p", "div[class^=exa][class$=mple]", "div p a", "div, p, a", ".note", "div.example", "ul .tocline2", "div.example, div.note", "#title", "h1#title", "div #title", "ul.toc li.tocline2", "ul.toc > li.tocline2", "h1#title + div > p", "h1[id]:contains(Selectors)", "a[href][lang][class]", "div[class]", "div[class=example]", "div[class^=exa]", "div[class$=mple]", "div[class*=e]", "div[class|=dialog]", "div[class!=made_up]", "div[class~=example]"/*, "div:not(.example)", "p:contains(selectors)", "p:nth-child(even)", "p:nth-child(2n)", "p:nth-child(odd)", "p:nth-child(2n+1)", "p:nth-child(n)", "p:only-child", "p:last-child", "p:first-child"*/];
 
-var engines = [function(a,b){return CSSselect.iterate(b,a);}, soupselect.select];
+var engines = [function(a,b){return CSSselect(b,a);}, soupselect.select];
 
 //returns true when an error occurs
 function testResult(rule, index){
