d98a40aa78400a4110a295758d96caf13212d48a
Felix Böhm <me@feedic.com>
2013-09-23 15:38:42 +0200

    [tests/qwery] :humanoid takes no arguments

--- test/qwery/index.js
+++ test/qwery/index.js
@@ -18,7 +18,7 @@ CSSselect.pseudos.target = function(elem) {
 	The following is taken from https://github.com/ded/qwery/blob/master/tests/tests.js
 */
 
-CSSselect.pseudos.humanoid = function(e, v) {
+CSSselect.pseudos.humanoid = function(e) {
     return CSSselect.is(e, "li:contains(human)") || CSSselect.is(e, "ol:contains(human)");
 };
 
