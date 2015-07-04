fb55/css-select

commit 0f0daad251094287a6223ba24467bf073dc2c0fe

compile: proper context support (finally)

lib/compile.js
@@ -31,35 +31,55 @@ function compileUnsafe(selector, options, context){
 	return compileToken(token, options, context);
 }
 
+function includesScopePseudo(t){
+    return t.type === "pseudo" && (
+        t.name === "scope" || (
+            Array.isArray(t.data) &&
+            t.data.some(function(data){
+                return data.some(includesScopePseudo);
+            })
+        )
+    );
+}
+
+var DESCENDANT_TOKEN = {type: "descendant"},
+    SCOPE_TOKEN = {type: "pseudo", name: "scope"},
+    PLACEHOLDER_ELEMENT = {},
+    getParent = DomUtils.getParent;
+
+//CSS 4 Spec (Draft): 3.3.1. Absolutizing a Scope-relative Selector
+//http://www.w3.org/TR/selectors4/#absolutizing
+function absolutize(token, context){
+    //TODO better check if context is document
+    var hasContext = !!context && context.every(function(e){
+        return e === PLACEHOLDER_ELEMENT || !!getParent(e);
+    });
+
+
+    token.forEach(function(t){
+        var addScope = t.length > 0 && isTraversal(t[0]) && t[0].type !== "descendant";
+
+        if(hasContext && !includesScopePseudo(t)){
+            t.unshift(DESCENDANT_TOKEN);
+            addScope = true;
+        }
+
+        if(addScope){
+            t.unshift(SCOPE_TOKEN);
+        }
+    });
+}
+
 function compileToken(token, options, context){
+    token = token.filter(function(t){ return t.length > 0; });
+
 	token.forEach(sortRules);
 
-	if(options && options.context){
-		var ctx = options.context;
+    context = (options && options.context) || context;
 
-		token.forEach(function(t){
-			if(!isTraversal(t[0])){
-				t.unshift({type: "descendant"});
-			}
-		});
+    if(context && !Array.isArray(context)) context = [context];
 
-		context = Array.isArray(ctx) ?
-			function(elem){
-				return ctx.indexOf(elem) >= 0;
-			} : function(elem){
-				return ctx === elem;
-			};
-
-		if(options.rootFunc){
-			var root = options.rootFunc;
-
-			options.rootFunc = function(elem){
-				return context(elem) && root(elem);
-			};
-		} else {
-			options.rootFunc = context;
-		}
-	}
+    absolutize(token, context);
 
 	return token
 		.map(function(rules){ return compileRules(rules, options, context); })
@@ -71,8 +91,6 @@ function isTraversal(t){
 }
 
 function compileRules(rules, options, context){
-	if(rules.length === 0) return falseFunc;
-
 	return rules.reduce(function(func, rule){
 		if(func === falseFunc) return func;
 		return Rules[rule.type](func, rule, options, context);

commit 74bba47a3789aabbe72e568a1586bc61f1619a4e

Add failing test to demonstrate regression
Commit 0f0daad251094287a6223ba24467bf073dc2c0fe introduced a breaking
change in this library's functionality: the selection API no longer
matches against elements specified via an array when those elements are
not root nodes.

There were previously no tests asserting this behavior, but the change
should be considered "breaking" for two reasons:

1. it is inconsistent--root nodes may still be queried in this way
2. the project's documentation implies the original behavior:

   > ### `CSSselect(query, elems, options)`
   >
   > Queries `elems`, returns an array containing all matches.
   >
   > - `query` can be either a CSS selector or a function.
   > - `elems` can be either an array of elements, or a single element. If
   >    it is an element, its children will be queried.
   > - `options` is described below.

Add one test that continues to work as expected (selecting from a root
node) and an equivalent test that was made to fail by the commit
mentioned above.

test/api.js
@@ -33,6 +33,17 @@ describe("API", function() {
         //probably more cases should be added here
     });
 
+    describe("selectAll", function() {
+        it("should query array elements directly when they have no parents", function() {
+            var divs = [dom];
+            assert.deepEqual(CSSselect("div", divs), divs);
+        });
+        it("should query array elements directly when they have parents", function() {
+            var ps = CSSselect("p", [dom]);
+            assert.deepEqual(CSSselect("p", ps), ps);
+        });
+    });
+
     describe("unsatisfiable and universally valid selectors", function() {
         it("in :not", function() {
             var func = CSSselect._compileUnsafe(":not(*)");