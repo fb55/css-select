e109d4b3668354542c806a6b652410f135d679ae
Nik Coughlin <nrkn.com@gmail.com>
2016-11-19 02:35:48 +1300

    Allow adapter to be passed in via options (#66)
    * WIP
    
    Still to do, finish browser-adapter tests - the adapter API and also
    that it works with CSS select with a handful of selectors (if a handful
    works likely the whole thing should).
    
    * Fix whitespace
    
    * Tests for adapter
    
    * Fix eol
    
    crlf -> lf
    
    * eslint
    
    * eslint
    
    * spaces in parens etc
    
    * eslint spaces in parens
    
    * Correct mixed tabs/spaces
    
    * adapter.getText -> getText, attribute rules inside closure
    
    * options guard, remove browser adapter
    
    * eslint
    
    * Fix eslint errors (#70)
    
    * rebase, eslint etc
    
    Signed-off-by: Nik Coughlin <nrkn.com@gmail.com>
    
    * Tests for adapter
    
    * spaces in parens etc
    
    * eslint spaces in parens
    
    * Correct mixed tabs/spaces
    
    * adapter.getText -> getText, attribute rules inside closure
    
    * options guard, remove browser adapter
    
    * eslint
    
    Fixes #71

--- index.js
+++ index.js
@@ -2,57 +2,61 @@
 
 module.exports = CSSselect;
 
-var Pseudos       = require("./lib/pseudos.js"),
-	DomUtils      = require("domutils"),
-	findOne       = DomUtils.findOne,
-	findAll       = DomUtils.findAll,
-	getChildren   = DomUtils.getChildren,
-	getSiblings   = DomUtils.getSiblings,
-	removeSubsets = DomUtils.removeSubsets,
-	falseFunc     = require("boolbase").falseFunc,
-	compile       = require("./lib/compile.js"),
-	compileUnsafe = compile.compileUnsafe,
-	compileToken  = compile.compileToken;
+var DomUtils       = require("domutils"),
+	falseFunc      = require("boolbase").falseFunc,
+	compileFactory = require("./lib/compile.js"),
+	defaultCompile = compileFactory(DomUtils);
+
+function adapterCompile(adapter){
+	return adapter === DomUtils ? defaultCompile : compileFactory(adapter);
+}
 
 function getSelectorFunc(searchFunc){
 	return function select(query, elems, options){
-		if(typeof query !== "function") query = compileUnsafe(query, options, elems);
-		if(query.shouldTestNextSiblings) elems = appendNextSiblings((options && options.context) || elems);
-		if(!Array.isArray(elems)) elems = getChildren(elems);
-		else elems = removeSubsets(elems);
-		return searchFunc(query, elems);
+		options = options || {}
+		options.adapter = options.adapter || DomUtils;
+		var compile = adapterCompile(options.adapter);
+
+		if(typeof query !== "function") query = compile.compileUnsafe(query, options, elems);
+		if(query.shouldTestNextSiblings) elems = appendNextSiblings((options && options.context) || elems, options.adapter);
+		if(!Array.isArray(elems)) elems = options.adapter.getChildren(elems);
+		else elems = options.adapter.removeSubsets(elems);
+		return searchFunc(query, elems, options);
 	};
 }
 
-function getNextSiblings(elem){
-	var siblings = getSiblings(elem);
+function getNextSiblings(elem, adapter){
+	var siblings = adapter.getSiblings(elem);
 	if(!Array.isArray(siblings)) return [];
 	siblings = siblings.slice(0);
 	while(siblings.shift() !== elem);
 	return siblings;
 }
 
-function appendNextSiblings(elems){
+function appendNextSiblings(elems, adapter){
 	// Order matters because jQuery seems to check the children before the siblings
 	if(!Array.isArray(elems)) elems = [elems];
 	var newElems = elems.slice(0);
 
 	for(var i = 0, len = elems.length; i < len; i++){
-		var nextSiblings = getNextSiblings(newElems[i]);
+		var nextSiblings = getNextSiblings(newElems[i], adapter);
 		newElems.push.apply(newElems, nextSiblings);
 	}
 	return newElems;
 }
 
-var selectAll = getSelectorFunc(function selectAll(query, elems){
-	return (query === falseFunc || !elems || elems.length === 0) ? [] : findAll(query, elems);
+var selectAll = getSelectorFunc(function selectAll(query, elems, options){
+	return (query === falseFunc || !elems || elems.length === 0) ? [] : options.adapter.findAll(query, elems);
 });
 
-var selectOne = getSelectorFunc(function selectOne(query, elems){
-	return (query === falseFunc || !elems || elems.length === 0) ? null : findOne(query, elems);
+var selectOne = getSelectorFunc(function selectOne(query, elems, options){
+	return (query === falseFunc || !elems || elems.length === 0) ? null : options.adapter.findOne(query, elems);
 });
 
 function is(elem, query, options){
+	options = options || {}
+	options.adapter = options.adapter || DomUtils;
+	var compile = adapterCompile(options.adapter);
 	return (typeof query === "function" ? query : compile(query, options))(elem);
 }
 
@@ -63,9 +67,9 @@ function CSSselect(query, elems, options){
 	return selectAll(query, elems, options);
 }
 
-CSSselect.compile = compile;
-CSSselect.filters = Pseudos.filters;
-CSSselect.pseudos = Pseudos.pseudos;
+CSSselect.compile = defaultCompile;
+CSSselect.filters = defaultCompile.Pseudos.filters;
+CSSselect.pseudos = defaultCompile.Pseudos.pseudos;
 
 CSSselect.selectAll = selectAll;
 CSSselect.selectOne = selectOne;
@@ -73,9 +77,9 @@ CSSselect.selectOne = selectOne;
 CSSselect.is = is;
 
 //legacy methods (might be removed)
-CSSselect.parse = compile;
+CSSselect.parse = defaultCompile;
 CSSselect.iterate = selectAll;
 
 //hooks
-CSSselect._compileUnsafe = compileUnsafe;
-CSSselect._compileToken = compileToken;
+CSSselect._compileUnsafe = defaultCompile.compileUnsafe;
+CSSselect._compileToken = defaultCompile.compileToken;
