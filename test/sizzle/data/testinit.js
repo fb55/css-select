464a9577dd8242ccaf7d519af2e2e7ca8725cba1
Felix Böhm <me@feedic.com>
2020-09-22 13:51:57 -0700

    Update testinit.js

--- test/sizzle/data/testinit.js
+++ test/sizzle/data/testinit.js
@@ -1,25 +1,10 @@
 const assert = require("assert");
-const decircularize = require("../../decircularize");
 const helper = require("../../tools/helper");
 const CSSselect = require("../../../src").default;
 const path = require("path");
 const docPath = path.join(__dirname, "index.html");
 let document = null;
 
-// In this module, the only use-case is to compare arrays of
-function deepEqual(a, b, msg) {
-    try {
-        assert.deepEqual(decircularize(a), decircularize(b), msg);
-    } catch (e) {
-        e.actual = JSON.stringify(a.map(getId), 0, 2);
-        e.expected = JSON.stringify(b.map(getId), 0, 2);
-        throw e;
-    }
-
-    function getId(n) {
-        return n && n.attribs.id;
-    }
-}
 
 function loadDoc() {
     return (document = helper.getDocument(docPath));
@@ -37,32 +22,23 @@ module.exports = {
  * @example q("main", "foo", "bar")
  * @result [<div id="main">, <span id="foo">, <input id="bar">]
  */
-function q(...args) {
-    const r = [];
-
-    for (let i = 0; i < args.length; i++) {
-        r.push(document.getElementById(args[i]));
-    }
-    return r;
+function q(...ids) {
+    return ids.map(id => document.getElementById(id))
 }
 
 /**
  * Asserts that a select matches the given IDs
- * @param {String} a - Assertion name
- * @param {String} b - Sizzle selector
- * @param {String} c - Array of ids to construct what is expected
+ * @param {String} assertionName - Assertion name
+ * @param {String} sizzleSelector - Sizzle selector
+ * @param {String} expectedIds - Array of ids to construct what is expected
  * @example t("Check for something", "//[a]", ["foo", "baar"]);
- * @result returns true if "//[a]" return two elements with the IDs 'foo' and 'baar'
+ * @returns `true` iff the selector produces the expected elements.
  */
-function t(a, b, c) {
-    const f = CSSselect(b, document);
-    let s = "";
+function t(assertionName, sizzleSelector, expectedIds) {
+    const actual = CSSselect(sizzleSelector, document);
+    const actualIds = actual.map(e => e.attribs.id);
 
-    for (let i = 0; i < f.length; i++) {
-        s += `${s && ","}"${f[i].id}"`;
-    }
-
-    deepEqual(f, q.apply(q, c), `${a} (${b})`);
+    assert.deepStrictEqual(actualIds, expectedIds, `${assertionName} (${sizzleSelector})`);
 }
 
 const xmlDoc = helper.getDOMFromPath(path.join(__dirname, "fries.xml"), {
