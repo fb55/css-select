187c9cd0d7bc9f1c870a2b18ab9601d76c672c04
Felix Boehm <me@feedic.com>
2020-12-01 15:09:20 -0800

    build(deps): bump nth-check from 1.0.2 to 2.0.0

--- src/declarations/nth-check.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "nth-check" {
-    export default function nthCheck(
-        formula: string
-    ): (position: number) => boolean;
-    export function parse(formula: string): [number, number];
-    export function compile(
-        parsed: [number, number]
-    ): (position: number) => boolean;
-}
